c1f016163c5bb8c88fd03232b76afde35c497d28
Chaithanya-DA <venkatachaithanya2002@gmail.com>
2025-06-11 06:51:03 +0530

    Fix: use relative path for model file

--- Prediction_Helper.py
+++ Prediction_Helper.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from joblib import load
 
-model_young = load(r"C:\Users\venka\OneDrive\Desktop\pythonpractice\statistics\App\artifacts\model_young.joblib")
+model_young = load("artifacts/model_young.joblib")
 model_rest = load(r"C:\Users\venka\OneDrive\Desktop\pythonpractice\statistics\App\artifacts\model_rest.joblib")
 
 
